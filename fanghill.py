4d0c229372c9d3583e4da1957b7f3cf3ee7cb6f5
suyang <suyang@haohandata.com.cn>
2024-02-23 14:31:02 +0800

    tick修改。

--- fanghill.py
+++ fanghill.py
@@ -1,8 +1,12 @@
-import os, time, datetime, logging, hashlib, json, requests
+import os, time, datetime, logging, hashlib, json, requests, logging
+
+logging.basicConfig(level=logging.INFO)
+console_handler = logging.StreamHandler()
+logging.getLogger().addHandler(console_handler)
 
 MID = 43447
 ORDER_WAY = 0
-TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEzNjkxMjYyODUzIiwiUm9sZUlkcyI6IiIsInJvbGUiOiIiLCJSZWFsTmFtZSI6IuiLj-aJrCIsIlVzZXJJZCI6IjM0MDQ3IiwiRGVwdElkIjoiMCIsIkRlcHRDb2RlIjoiIiwiRGVwdE5hbWUiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDI0LzgvMSAxOjA0OjEzIiwibmJmIjoxNjkwOTM4MjUzLCJleHAiOjE3MjI0NzQyNTMsImlhdCI6MTY5MDkzODI1MywiaXNzIjoiaG9uZ3hpbiIsImF1ZCI6Imhvbmd4aW4ifQ.tQzxkVJkbVJ3GD95XSqDQO9RESIlcnVd_UAiWlP9uyY"
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEzNjkxMjYyODUzIiwiUm9sZUlkcyI6IiIsInJvbGUiOiIiLCJSZWFsTmFtZSI6IuiLj-aJrCIsIlVzZXJJZCI6IjM0MDQ3IiwiRGVwdElkIjoiMCIsIkRlcHRDb2RlIjoiIiwiRGVwdE5hbWUiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDI1LzIvMjIgMTo1NzowMiIsIm5iZiI6MTcwODY1MzQyMiwiZXhwIjoxNzQwMTg5NDIyLCJpYXQiOjE3MDg2NTM0MjIsImlzcyI6Imhvbmd4aW4iLCJhdWQiOiJob25neGluIn0.CXZxLFWg0F0M3QO0KOYlsRaHDWnWmkIGSfB7_sLcp4A"
 URL = "https://tsg.fscac.org:5134/api/FW_Module/AddOrder"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 START_TIME = "2024-02-17 12:00:00"
@@ -22,8 +26,7 @@ def sign(map: dict) -> str:
             urlParams += f"{value}={map[value]}&"
     return hashlib.md5(urlParams.encode("utf-8")).hexdigest()
 
-
-def run():
+def post_server():
     timestamp = int(time.time() * 1000)
     data = {
         "Mid": MID,
@@ -38,20 +41,29 @@ def run():
     headers = {
         'Content-Type': 'application/json',
         'Sign': sign(newData),
-        'Timestamps': timestamp,
+        'Timestamps': str(timestamp),
         'User-Agent': USER_AGENT,
         'Authorization': f"Bearer {TOKEN}"
     }
     body = json.dumps(data)
+    logging.log(logging.INFO, f"post {URL} {headers} {body}")
     response = requests.post(URL, headers=headers, data=body).json()
     logging.log(logging.INFO, response)
+    if response["result"] == 1 or response["result"] == 0:
+        return True
+    return False
+
+def run_tick():
+    while True:
+        if post_server():
+            break
+        time.sleep(1)
 
 
 while True:
     dt = datetime.datetime.strptime(START_TIME, '%Y-%m-%d %H:%M:%S') - datetime.timedelta(minutes=1)
     if datetime.datetime.now() > dt:
         logging.log(logging.INFO, "开始...")
-        run()
-        break
+        run_tick()
     time.sleep(60)
     logging.log(logging.INFO, f"时间未到...{time.time()}")
