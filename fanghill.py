d813881e5495a2b7f62712ec07150e2073e39cc1
suyang <suyang@haohandata.com.cn>
2024-07-18 10:02:19 +0800

    修改。

--- fanghill.py
+++ fanghill.py
@@ -4,15 +4,15 @@ logging.basicConfig(level=logging.INFO)
 console_handler = logging.StreamHandler()
 logging.getLogger().addHandler(console_handler)
 
-MID = 45354
+MID = 45391
 ORDER_WAY = 0
 TOKENS = {
     "suyang": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEzNjkxMjYyODUzIiwiUm9sZUlkcyI6IiIsInJvbGUiOiIiLCJSZWFsTmFtZSI6IuiLj-aJrCIsIlVzZXJJZCI6IjM0MDQ3IiwiRGVwdElkIjoiMCIsIkRlcHRDb2RlIjoiIiwiRGVwdE5hbWUiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDI1LzIvMjIgMTo1NzowMiIsIm5iZiI6MTcwODY1MzQyMiwiZXhwIjoxNzQwMTg5NDIyLCJpYXQiOjE3MDg2NTM0MjIsImlzcyI6Imhvbmd4aW4iLCJhdWQiOiJob25neGluIn0.CXZxLFWg0F0M3QO0KOYlsRaHDWnWmkIGSfB7_sLcp4A",
-    "wufang": ""
+    "wufang": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjE4NzAxMTE2ODQ2IiwiUm9sZUlkcyI6IiIsInJvbGUiOiIiLCJSZWFsTmFtZSI6IuWQtOiKsyIsIlVzZXJJZCI6IjUyODEyIiwiRGVwdElkIjoiMCIsIkRlcHRDb2RlIjoiIiwiRGVwdE5hbWUiOiIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDI1LzcvMTggMTozNTowMSIsIm5iZiI6MTcyMTI2NjUwMSwiZXhwIjoxNzUyODAyNTAxLCJpYXQiOjE3MjEyNjY1MDEsImlzcyI6Imhvbmd4aW4iLCJhdWQiOiJob25neGluIn0.UevQttiGIIF6TE_XRxP_tR1KTlLW2A538_ACEVvrdp8"
 }
 URL = "https://tsg.fscac.org:5134/api/FW_Module/AddOrder"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
-START_TIME = "2024-07-11 10:00:00"
+START_TIME = "2024-07-18 10:00:00"
 
 
 def sign(map: dict) -> str:
@@ -75,20 +75,20 @@ def timestamp_to_date(time_stamp, format_string="%Y-%m-%d %H:%M:%S"):
     return other_style_time
 
 
-def run(token: str):
+def run(name, token):
     while True:
         dt = datetime.datetime.strptime(START_TIME, '%Y-%m-%d %H:%M:%S') - datetime.timedelta(minutes=1)
         if datetime.datetime.now() > dt:
-            logging.log(logging.INFO, "开始...")
+            logging.log(logging.INFO, "开始..." + name)
             run_tick(token)
         time.sleep(60)
         logging.log(logging.INFO,
-                    f"时间未到...{datetime.datetime.strptime(START_TIME, '%Y-%m-%d %H:%M:%S')}/{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+                    f"时间未到...{name}:{datetime.datetime.strptime(START_TIME, '%Y-%m-%d %H:%M:%S')}/{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 threads = []
 for key, value in TOKENS.items():
-    thread = threading.Thread(target=run, name="", args=(value))
+    thread = threading.Thread(target=run, args=(key, value))
     thread.start()
     threads.append(thread)
 
