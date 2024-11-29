da2e2b389f449c95ff9b760129f723ddb076f645
suyang <suyang@haohandata.com.cn>
2024-11-29 17:50:15 +0800

    学习

--- backtrader/01.hello.py
+++ backtrader/01.hello.py
@@ -4,22 +4,18 @@ import numpy as np
 import datetime
 from copy import deepcopy
 
+# data部分
+# 开盘价（ Open）、最高价（High）、最低价（Low）、收盘价（Close）、成交量（Volume）、持仓量（OpenInterest）
+
 # 实例化 cerebro
 cerebro = bt.Cerebro()
+
+# 设置初始资金100000
+cerebro.broker.setcash(100000.0)
+
 # 打印初始资金
-print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+print('组合期初资金: %.2f' % cerebro.broker.getvalue())
 # 启动回测
 cerebro.run()
 # 打印回测完成后的资金
-print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-print("=============================")
-
-daily_price = pd.read_csv("./data/daily_price.csv", parse_dates=['datetime'])
-print(daily_price)
-
-print("=============================")
-result = daily_price.query("sec_code=='600466.SH'")
-print(result)
-
-print("=============================")
+print('组合期末资金: %.2f' % cerebro.broker.getvalue())
